ae2bc7e5f18b09819438a4afcc91052caf555b60
Snischuk Maxim <m.snischuk@gmail.com>
2024-04-26 19:28:05 +0300

    feat: implement header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { About } from './components/About/About';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 
+import styles from './app.module.css';
+
 function App() {
     return (
         <>
             <Header />
-            <main>
+            <main className={styles.main}>
                 <About />
             </main>
             <Footer />
