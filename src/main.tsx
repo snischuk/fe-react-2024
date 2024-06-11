f84b1eb5fbcd758086e3f7152f78a53d37bd99e8
Snischuk Maxim <m.snischuk@gmail.com>
2024-06-11 15:07:23 +0300

    fix: change BrowserRouter to HashRouter for correct work in gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './App.tsx';
 
@@ -8,8 +8,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
     <StrictMode>
-        <BrowserRouter basename="/fe-react-2024">
+        <HashRouter basename="/fe-react-2024">
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>,
 );
