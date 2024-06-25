7b07ec6f012ccd8ce5c5151f59a8c8fee7f1f94c
Snischuk Maxim <m.snischuk@gmail.com>
2024-06-25 13:15:46 +0300

    fix: aligned center not-found page

--- src/routes/NotFoundPage/NotFoundPage.tsx
+++ src/routes/NotFoundPage/NotFoundPage.tsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 import styles from './NotFoundPage.module.css';
 
 const NotFoundPage: FC = () => (
-    <>
+    <div className={styles.notFoundPage}>
         <p className={styles.notFoundPageInfo}>Oops! Page not found... :(</p>
         <Link className={styles.notFoundPageLink} to="/">
             Go to About page.
         </Link>
-    </>
+    </div>
 );
 
 export { NotFoundPage };
